25b33ae573350f01c11448af3a1a2bc540603244
SkanderGasmi <skandergasmi7@gmail.com>
2026-02-02 18:19:47 +0100

    fix

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -1,12 +1,11 @@
 package com.project.back_end.mvc;
 
-import com.project.back_end.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import java.util.Map;
+
+import com.project.back_end.services.TokenService;
 
 @Controller
 public class DashboardController {
@@ -18,10 +17,10 @@ public class DashboardController {
     // 3. Admin dashboard endpoint
     @GetMapping("/adminDashboard/{token}")
     public String adminDashboard(@PathVariable("token") String token) {
-        Map<String, Object> validation = tokenService.validateToken(token, "admin");
+        boolean valid = tokenService.validateToken(token, "admin");
 
         // If the map is empty, token is valid
-        if (validation.isEmpty()) {
+        if (valid) {
             return "admin/adminDashboard"; // Thymeleaf template path
         } else {
             return "redirect:/"; // Redirect to login/home page if token invalid
@@ -31,10 +30,10 @@ public class DashboardController {
     // 4. Doctor dashboard endpoint
     @GetMapping("/doctorDashboard/{token}")
     public String doctorDashboard(@PathVariable("token") String token) {
-        Map<String, Object> validation = tokenService.validateToken(token, "doctor");
+        boolean valid = tokenService.validateToken(token, "doctor");
 
         // If the map is empty, token is valid
-        if (validation.isEmpty()) {
+        if (valid) {
             return "doctor/doctorDashboard"; // Thymeleaf template path
         } else {
             return "redirect:/"; // Redirect to login/home page if token invalid
